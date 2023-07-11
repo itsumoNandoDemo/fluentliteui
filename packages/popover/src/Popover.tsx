f22f9f9508a8203aac6c229241e607118ecf027e
ArcadeKasagawa <kasagawa@qq.com>
2023-07-11 22:23:39 +0800

    update

--- packages/popover/src/Popover.tsx
+++ packages/popover/src/Popover.tsx
@@ -10,32 +10,36 @@ export const Popover = (props: Props) => {
     const { content, children, pop: popHTMLElement, ...rest } = props;
     const [show, setShow] = useState<boolean>(false);
     const [x, setX] = useState<number>(0);
-    const pop: any = useRef();
     const popwapper: any = useRef();
+    const pop: any = useRef();
 
-    const { top, right, bottom, left } = pop.current?.getBoundingClientRect?.() ?? {
+    const { top, right, bottom, left } = popwapper.current?.getBoundingClientRect?.() ?? {
         top: 0,
         right: 0,
         bottom: 0,
         left: 0,
     };
 
-    const { left: a, right: b } = popwapper.current?.getBoundingClientRect?.() ?? {
+    const { top: t, right: r, bottom: b, left: l } = pop.current?.getBoundingClientRect?.() ?? {
         top: 0,
         right: 0,
         bottom: 0,
         left: 0,
     };
+
+
     return (
-        <div {...rest} className="fluentliteui popover" ref={popwapper}>
-            <div className='popover-hover' onMouseOver={({ clientX, clientY }) => {
-                setX(clientX);
-            }}>{children}</div>
-            <div className="popover-pop" ref={pop} style={{ left: `${(right - left) / 2 * -1 + (b - a) / 2}px` }}>
-                <div className="popover-content">
-                    {popHTMLElement}
+        <div {...rest}>
+            <div className="fluentliteui popover" ref={popwapper}>
+                <div className='popover-hover' onMouseOver={({ clientX, clientY }) => {
+                    setX(clientX);
+                }}>{children}</div>
+                <div className="popover-pop" ref={pop} style={{ left: `${left + (right - left) / 2 - (r - l) / 2}px`, top: `${top - (b - t)}px` }}>
+                    <div className="popover-content">
+                        {popHTMLElement}
+                    </div>
+                    <div className="popover-arrow"></div>
                 </div>
-                <div className="popover-arrow"></div>
             </div>
         </div>
     );
